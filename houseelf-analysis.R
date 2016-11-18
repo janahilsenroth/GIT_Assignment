fd44a72b0056dea44942efbc911741576fda210d
Jana Hilsenroth <jhuebner@ufl.edu>
2016-11-18 12:49:54 -0500

    Import data

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -1,14 +1,15 @@
-get_data <- functin(){
-  read.csv("houseelf-analysis.R")
-}
+# Problem 3- Import data
+data <- read.csv("data/houseelf-earlength-dna-data.csv")
+
 
 #is this working?
 
 library(stringr)
 get_GC_content <- function(dna){
-lows <- str_to_lower(dna)
-c <- str_count(lows, "c")
-g <- str_count(lows, "g")
-content <- ((g+c)/str_length(lows))*100
-return(content)
+  lows <- str_to_lower(dna)
+  c <- str_count(lows, "c")
+  g <- str_count(lows, "g")
+  content <- ((g+c)/str_length(lows))*100
+  return(content)
 }
+
